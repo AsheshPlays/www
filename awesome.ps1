edfcbfec03fcd38da15e89866eb621669e8d67b4
AsheshPlays <118698151+AsheshPlays@users.noreply.github.com>
2024-05-01 17:11:54 +0800

    update

--- awesome.ps1
+++ awesome.ps1
@@ -1,6 +1,5 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
-Add-Type -AssemblyName Microsoft.VisualBasic
 
 function Install-Chocolatey {
     if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
@@ -38,24 +37,26 @@ function Clean-And-Exit {
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Ashesh Development: Tool Menu'
-$form.Size = New-Object System.Drawing.Size(800,600)
+$form.Size = New-Object System.Drawing.Size(500, 400)
 $form.StartPosition = 'CenterScreen'
 $form.Font = New-Object System.Drawing.Font('Consolas', 10)
 $form.BackColor = [System.Drawing.Color]::FromArgb(30, 30, 30)
 $form.ForeColor = [System.Drawing.Color]::FromArgb(0, 255, 0)
+$form.FormBorderStyle = 'FixedDialog'
+$form.MaximizeBox = $false
 
 $panel = New-Object System.Windows.Forms.Panel
-$panel.Location = New-Object System.Drawing.Point(10, 50)
-$panel.Size = New-Object System.Drawing.Size(760, 400)
+$panel.Location = New-Object System.Drawing.Point(10, 10)
+$panel.Size = New-Object System.Drawing.Size(480, 260)
 $panel.AutoScroll = $true
 
-$y = 10
 $applications = 'firefox, vlc, 7zip, git, vscode, nodejs, discord, slack, skype, chrome, edge'
+$y = 10
 foreach ($app in $applications.Split(', ')) {
     $checkBox = New-Object System.Windows.Forms.CheckBox
     $checkBox.Text = $app.Trim()
     $checkBox.Location = New-Object System.Drawing.Point(10, $y)
-    $checkBox.Size = New-Object System.Drawing.Size(200, 20)
+    $checkBox.Size = New-Object System.Drawing.Size(450, 20)
     $checkBox.Tag = $app.Trim()
     $panel.Controls.Add($checkBox)
     $y += 30
@@ -63,26 +64,26 @@ foreach ($app in $applications.Split(', ')) {
 
 $buttonGetInstalled = New-Object System.Windows.Forms.Button
 $buttonGetInstalled.Text = 'Get Installed Apps'
-$buttonGetInstalled.Location = New-Object System.Drawing.Point(10, 460)
+$buttonGetInstalled.Location = New-Object System.Drawing.Point(10, 280)
 $buttonGetInstalled.Size = New-Object System.Drawing.Size(150, 30)
 $buttonGetInstalled.Add_Click({ Get-InstalledApps })
 
 $buttonInstallSelected = New-Object System.Windows.Forms.Button
 $buttonInstallSelected.Text = 'Install Selected Apps'
-$buttonInstallSelected.Location = New-Object System.Drawing.Point(170, 460)
+$buttonInstallSelected.Location = New-Object System.Drawing.Point(170, 280)
 $buttonInstallSelected.Size = New-Object System.Drawing.Size(150, 30)
 $buttonInstallSelected.Add_Click({ Install-SelectedApps })
 
 $buttonUninstallSelected = New-Object System.Windows.Forms.Button
 $buttonUninstallSelected.Text = 'Uninstall Selected Apps'
-$buttonUninstallSelected.Location = New-Object System.Drawing.Point(330, 460)
+$buttonUninstallSelected.Location = New-Object System.Drawing.Point(330, 280)
 $buttonUninstallSelected.Size = New-Object System.Drawing.Size(150, 30)
 $buttonUninstallSelected.Add_Click({ Uninstall-SelectedApps })
 
 $buttonCleanExit = New-Object System.Windows.Forms.Button
 $buttonCleanExit.Text = 'Clean and Exit'
-$buttonCleanExit.Location = New-Object System.Drawing.Point(490, 460)
-$buttonCleanExit.Size = New-Object System.Drawing.Size(150, 30)
+$buttonCleanExit.Location = New-Object System.Drawing.Point(10, 320)
+$buttonCleanExit.Size = New-Object System.Drawing.Size(470, 30)
 $buttonCleanExit.Add_Click({ Clean-And-Exit })
 
 $form.Controls.Add($panel)
