957dc6e2e02eeda08535e303d3c5f76844b5ddc3
AsheshPlays <118698151+AsheshPlays@users.noreply.github.com>
2024-05-01 16:43:46 +0800

    update

--- awesome.ps1
+++ awesome.ps1
@@ -1,19 +1,15 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# Check and install Chocolatey if not installed
 function Install-Chocolatey {
     if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-        Write-Host "Installing Chocolatey..."
         Start-Process powershell -ArgumentList 'Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"))' -Verb RunAs -Wait
     }
-    Write-Host "Chocolatey is installed."
 }
 
-# Install selected applications
 function Install-SelectedApps {
     Install-Chocolatey
-    $selectedApps | ForEach-Object {
+    $global:selectedApps | ForEach-Object {
         Start-Process powershell -ArgumentList "choco install $_ -y" -Verb RunAs -Wait
     }
 }
@@ -23,6 +19,8 @@ $form.Text = 'Ashesh Development Windows Tool Menu'
 $form.Size = New-Object System.Drawing.Size(800,600)
 $form.StartPosition = 'CenterScreen'
 $form.Font = New-Object System.Drawing.Font('Consolas', 10)
+$form.BackColor = [System.Drawing.Color]::FromArgb(30, 30, 30)
+$form.ForeColor = [System.Drawing.Color]::FromArgb(0, 255, 0)
 
 $categories = @{
     'General Applications' = 'firefox, vlc, 7zip';
@@ -31,7 +29,6 @@ $categories = @{
     'Browsers' = 'chrome, edge, firefox';
 }
 
-# Create checkboxes dynamically based on categories
 $y = 20
 foreach ($category in $categories.Keys) {
     $label = New-Object System.Windows.Forms.Label
@@ -57,15 +54,12 @@ $installButton.Text = 'Install Selected Applications'
 $installButton.Location = New-Object System.Drawing.Point(30, $y + 20)
 $installButton.Size = New-Object System.Drawing.Size(200, 30)
 $installButton.Add_Click({
-    $selectedApps = @()
+    $global:selectedApps = @()
     $form.Controls | Where-Object {$_ -is [System.Windows.Forms.CheckBox] -and $_.Checked} | ForEach-Object {
-        $selectedApps += $_.Tag
+        $global:selectedApps += $_.Tag
     }
     Install-SelectedApps
 })
 $form.Controls.Add($installButton)
-
-$form.BackColor = [System.Drawing.Color]::FromArgb(30, 30, 30)
-$form.ForeColor = [System.Drawing.Color]::FromArgb(0, 255, 0)
 
 $form.ShowDialog()
